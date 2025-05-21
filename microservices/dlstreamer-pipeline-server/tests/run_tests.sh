5d9996dd54ad4e4743b42842899488948f8f202d
Sowmya Ramanchandran <sowmya.ramachandran@intel.com>
2025-05-21 16:17:29 +0530

    [DL Streamer Pipeline Server] PR workflow for build, test and SDLE scans (#173)
    Co-authored-by: Ashish Jagadish <ashish.jagadish@intel.com>

--- microservices/dlstreamer-pipeline-server/tests/run_tests.sh
+++ microservices/dlstreamer-pipeline-server/tests/run_tests.sh
@@ -4,5 +4,8 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-pytest -vv --cov=. tests/ --cov-config=tests/.coveragerc
-coverage xml -i -o /tmp/coverage.xml
+pytest -vv --cov=. tests/ --cov-config=tests/.coveragerc 
+exit_code=$?
+coverage html -i -d /tmp/htmlcov
+# Exit with pytest's exit code
+exit $exit_code
